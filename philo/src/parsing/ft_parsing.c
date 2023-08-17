74ddcae6e8f799c1490bf514d4a63a2270d9eb22
ThibaultNGUYEN26 <thibaultnguyen26@github.com>
2023-08-17 14:57:08 +0200

    Try repush

--- philo/src/parsing/ft_parsing.c
+++ philo/src/parsing/ft_parsing.c
@@ -6,17 +6,29 @@
 /*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 14:34:47 by thibnguy          #+#    #+#             */
-/*   Updated: 2023/08/14 20:03:40 by thibnguy         ###   ########.fr       */
+/*   Updated: 2023/08/16 13:40:06 by thibnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
+
+static	int	ft_check_nb_arg(int argc)
+{
+	if (argc == 1)
+	{
+		printf(GREEN"[USING] : ./philo [nb_philo] [time_to_die] \
+[time_to_eat] [time_to_sleep] [(nb_of_times_each_philo_must_eat)] \n"EOC);
+		return (0);
+	}
+	return (1);
+}
 
 int	ft_parsing(int argc, char *argv[])
 {
 	int	i;
 	int	j;
 
+	return (ft_check_nb_arg(argc));
 	i = 0;
 	while (++i < argc)
 	{
@@ -25,7 +37,7 @@ int	ft_parsing(int argc, char *argv[])
 		{
 			if (ft_strlen(argv[i]) > 11 || ft_atoi(argv[i])
 				> INT_MAX || !ft_isdigit(argv[i][j])
-				|| argv[i][j] == '-' || argc < 5 || argc > 6)
+				|| argv[i][j] == '-' || argc < 5 || argc > 6 || argc == 0)
 			{
 				printf(GREEN"[USING] : ./philo [nb_philo] [time_to_die] \
 [time_to_eat] [time_to_sleep] [(nb_of_times_each_philo_must_eat)] \n"EOC);
@@ -33,7 +45,7 @@ int	ft_parsing(int argc, char *argv[])
 			}
 		}
 	}
-	if (ft_atoi(argv[1]) == 0 || (argc == 6 && ft_atoi(argv[5]) == 0))
+	if ((ft_atoi(argv[1]) == 0 || (argc == 6 && ft_atoi(argv[5]) == 0)))
 		return (0);
 	return (1);
 }
--- philo/src/parsing/ft_printf.c
+++ philo/src/parsing/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 17:33:16 by thibnguy          #+#    #+#             */
-/*   Updated: 2023/08/14 20:03:04 by thibnguy         ###   ########.fr       */
+/*   Updated: 2023/08/16 11:51:57 by thibnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	ft_printf(t_data *data, char *msg, long long int arg1, int arg2)
 	pthread_mutex_lock(&(data->print));
 	if (ft_strstr(msg, "eating"))
 	{
-		printf(WHITE"%lld\t%d\t"YELLOW"has taken a fork\n", arg1, arg2);
-		printf(WHITE"%lld\t%d\t"RED"is eating", arg1, arg2);
+		printf(WHITE"%lld\t%d\t"YELLOW"has taken a fork\n", arg1, arg2 + 1);
+		printf(WHITE"%lld\t%d\t"RED"is eating", arg1, arg2 + 1);
 	}
 	else
 	{
-		printf(WHITE"%lld\t%d\t", arg1, arg2);
+		printf(WHITE"%lld\t%d\t", arg1, arg2 + 1);
 		if (ft_strstr(msg, "fork"))
 			printf(YELLOW"%s", msg);
 		else if (ft_strstr(msg, "thinking"))
